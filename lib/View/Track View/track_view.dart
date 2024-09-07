e03ed55a6f783d6d9ae5f4e0860af3a34c460f26
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-09-06 18:10:42 -0700

    Updated Code of Track View

--- lib/View/Track View/track_view.dart
+++ lib/View/Track View/track_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:music_app/Custom%20Widgets/bottom_bar.dart';
+import 'package:music_app/Services/global_data.dart';
 import 'package:music_app/Services/utils.dart';
 import 'package:music_app/View/Track%20View/track_viewmodel.dart';
 import 'package:provider/provider.dart';
@@ -25,6 +26,7 @@ class _TrackViewState extends State<TrackView> {
     final model = Provider.of<TrackViewmodel>(context, listen: false);
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       backgroundColor: Utils.blackPrimary,
       bottomNavigationBar:
@@ -37,6 +39,7 @@ class _TrackViewState extends State<TrackView> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            // Header section
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -61,9 +64,9 @@ class _TrackViewState extends State<TrackView> {
                 )
               ],
             ),
-            SizedBox(
-              height: screenHeight * 0.030,
-            ),
+            SizedBox(height: screenHeight * 0.030),
+
+            // Artist image
             Padding(
               padding: EdgeInsets.only(
                   left: screenWidth * 0.100, right: screenWidth * 0.080),
@@ -73,39 +76,93 @@ class _TrackViewState extends State<TrackView> {
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(screenWidth * 0.020),
                   image: DecorationImage(
-                      image: NetworkImage(
-                        widget.artist_Pic.toString(),
-                      ),
-                      fit: BoxFit.fill),
+                      image: NetworkImage(widget.artist_Pic), fit: BoxFit.fill),
                 ),
               ),
             ),
-            SizedBox(
-              height: screenHeight * 0.030,
-            ),
+            SizedBox(height: screenHeight * 0.030),
+
+            // Artist name and genres
             Center(
-                child: Column(
-              children: [
-                Text(
-                  widget.artist_Name.toString(),
-                  style: TextStyle(
-                      color: Utils.white,
-                      fontSize: screenHeight * 0.030,
-                      fontFamily: 'Century Gothic Bold'),
-                ),
-                SizedBox(
-                  height: screenHeight * 0.010,
-                ),
-                Text(
-                  '${widget.artist_genres[0]}, ${widget.artist_genres[1]}',
-                  style: TextStyle(
-                      color: Utils.white.withOpacity(0.6),
-                      fontFamily: 'Century Gothic Bold',
-                      fontSize: screenHeight * 0.020),
-                ),
-              ],
-            )),
-            
+              child: Column(
+                children: [
+                  Text(
+                    widget.artist_Name,
+                    style: TextStyle(
+                        color: Utils.white,
+                        fontSize: screenHeight * 0.030,
+                        fontFamily: 'Century Gothic Bold'),
+                  ),
+                  SizedBox(height: screenHeight * 0.010),
+                  Text(
+                    '${widget.artist_genres[0]}, ${widget.artist_genres[1]}',
+                    style: TextStyle(
+                        color: Utils.white.withOpacity(0.6),
+                        fontFamily: 'Century Gothic Bold',
+                        fontSize: screenHeight * 0.020),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: screenHeight * 0.030),
+
+            // Track list section
+            Expanded(
+              child: GlobalData.artistTracks.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: GlobalData.artistTracks.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: screenHeight * 0.010),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                height: screenHeight * 0.060,
+                                width: screenHeight * 0.060, // Add width here
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: NetworkImage(GlobalData
+                                          .artistTracks[index]['Image']
+                                          .toString()),
+                                      fit: BoxFit.fill),
+                                ),
+                              ),
+                              SizedBox(width: screenWidth * 0.040),
+                              GlobalData.artistTracks[index]['Track Name'] ==
+                                      'COME THROUGH (feat. Abdullah Maharvi & Talha Anjum)'
+                                  ? Text(
+                                      'COME THROUGH'.toString(),
+                                      style: TextStyle(
+                                        color: Utils.white,
+                                        fontSize: screenHeight * 0.018,
+                                      ),
+                                    )
+                                  : Text(
+                                      GlobalData.artistTracks[index]
+                                              ['Track Name']
+                                          .toString(),
+                                      style: TextStyle(
+                                        color: Utils.white,
+                                        fontSize: screenHeight * 0.018,
+                                      ),
+                                    ),
+                            ],
+                          ),
+                        );
+                      },
+                    )
+                  : Center(
+                      child: Text(
+                        'No tracks available',
+                        style: TextStyle(
+                          color: Utils.white.withOpacity(0.5),
+                          fontSize: screenHeight * 0.022,
+                        ),
+                      ),
+                    ),
+            ),
           ],
         ),
       ),
